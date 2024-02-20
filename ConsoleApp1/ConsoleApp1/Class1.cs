117c8772a1f521002e5f098b59fe31e8b7ac05ae
Bailey Long <bailey-long@live.nmit.ac.nz>
2024-02-21 10:48:13 +1300

    added a console app to test c# with

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        string _name = string.Empty;
+
+        public string name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public Class1(string name) 
+        { 
+            _name = name;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp1;
+
+/*var class1 = new Class1("Bailey");
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(class1.name);*/
+
+byte myValue = 64;
+double myDouble = 295.1; //change to 295.1
+try
+{
+    myValue = (byte)myDouble;
+    Console.WriteLine(myValue);
+}
+catch (OverflowException)
+{
+    Console.WriteLine("{0} is out of range of a byte.", myValue);
+}
+//Outputs 5 
